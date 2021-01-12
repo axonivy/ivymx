f7102f689d5989dbb2c95997c69e27ca46dddfa8
Andreas Rusch <andreas.rusch@axonivy.com>
2021-01-12 12:51:20 +0100

    XIVY-5284 Fix tests after changing default error strategy

--- src/test/java/com/axonivy/jmx/TestRegisterErrorStrategy.java
+++ src/test/java/com/axonivy/jmx/TestRegisterErrorStrategy.java
@@ -1,6 +1,7 @@
 package com.axonivy.jmx;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import javax.management.NotCompliantMBeanException;
 
@@ -10,18 +11,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.axonivy.jmx.MAttribute;
-import com.axonivy.jmx.MBean;
-import com.axonivy.jmx.MBeans;
-import com.axonivy.jmx.MConstants;
-import com.axonivy.jmx.MException;
 import com.axonivy.jmx.internal.LogErrorStrategy;
 import com.axonivy.jmx.util.LogTestAppender;
 
 public class TestRegisterErrorStrategy
 {
   private static final TestBean TEST_BEAN = new TestBean();
-  private final LogTestAppender LOG_APPENDER = new LogTestAppender(Level.ERROR);
+  private final LogTestAppender logAppender = new LogTestAppender(Level.ERROR);
 
   @MBean(value="Test:type=TestType")
   public static class TestBean
@@ -37,7 +33,7 @@ public class TestRegisterErrorStrategy
   @Before
   public void before()
   {
-    Logger.getLogger(LogErrorStrategy.class).addAppender(LOG_APPENDER);
+    Logger.getLogger(LogErrorStrategy.class).addAppender(logAppender);
   }
 
   @After
@@ -47,34 +43,37 @@ public class TestRegisterErrorStrategy
     MBeans.setRegisterMBeanErrorStrategy(MConstants.DEFAULT_ERROR_STRATEGY);
   }
 
-  @Test(expected=MException.class)
+  @Test
   public void testDefaultStrategy()
   {
+    assertThat(logAppender.getRecording()).isEmpty();
     MBeans.registerMBeanFor(TEST_BEAN);
+    assertThat(logAppender.getRecording()).contains("Could not register MBean '"+TEST_BEAN+"'");
+    assertThat(logAppender.getRecording()).contains(NotCompliantMBeanException.class.getName());
   }
 
-  @Test(expected=MException.class)
+  @Test
   public void testThrowRuntimeExceptionStrategy()
   {
     MBeans.setRegisterMBeanErrorStrategy(MConstants.THROW_RUNTIME_EXCEPTION_ERROR_STRATEGY);
-    MBeans.registerMBeanFor(TEST_BEAN);
+    assertThatThrownBy(() -> MBeans.registerMBeanFor(TEST_BEAN)).isInstanceOf(MException.class);
   }
 
   @Test
   public void testLogErrorStrategy()
   {
     MBeans.setRegisterMBeanErrorStrategy(MConstants.LOG_ERROR_STRATEGY);
-    assertThat(LOG_APPENDER.getRecording()).isEmpty();
+    assertThat(logAppender.getRecording()).isEmpty();
     MBeans.registerMBeanFor(TEST_BEAN);
-    assertThat(LOG_APPENDER.getRecording()).contains("Could not register MBean '"+TEST_BEAN+"'");
-    assertThat(LOG_APPENDER.getRecording()).contains(NotCompliantMBeanException.class.getName());
+    assertThat(logAppender.getRecording()).contains("Could not register MBean '"+TEST_BEAN+"'");
+    assertThat(logAppender.getRecording()).contains(NotCompliantMBeanException.class.getName());
   }
 
   @Test
   public void testIgnoreErrorStrategy()
   {
     MBeans.setRegisterMBeanErrorStrategy(MConstants.IGNORE_ERROR_STRATEGY);
-    assertThat(LOG_APPENDER.getRecording()).isEmpty();
+    assertThat(logAppender.getRecording()).isEmpty();
   }
 
 }
