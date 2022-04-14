cfb5a804c77fc2555e693c1b46f295db264f8d2f
Reto Weiss <reto.weiss@axonivy.com>
2022-04-14 12:00:11 +0200

    Fix #28 First unregister and old bean before register the new one

--- src/test/java/com/axonivy/jmx/TestMMap.java
+++ src/test/java/com/axonivy/jmx/TestMMap.java
@@ -1,9 +1,12 @@
 package com.axonivy.jmx;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,19 +25,24 @@ public class TestMMap extends BaseMTest<TestMMap.TestBean>
       return ""+id;
     }
   }
-  
+
+  @MBean(value="Test:name=Hello")
+  public static class HelloBean extends TestBean
+  {
+  }
+
   public TestMMap() throws MalformedObjectNameException
   {
     super(new TestBean(), "Test:type=TestType,id="+(TestBean.global-1));
   }
-  
+
   @Override
   @Before
   public void before()
   {
     // Do not register test bean by default
   }
-  
+
   @Test
   public void testPut()
   {
@@ -50,7 +58,16 @@ public class TestMMap extends BaseMTest<TestMMap.TestBean>
     assertNotRegistered();
   }
 
-  
+  @Test
+  public void testPut2SameName() throws MalformedObjectNameException
+  {
+    HelloBean bean1 = new HelloBean();
+    HelloBean bean2 = new HelloBean();
+    testMap.put("blah", bean1);
+    testMap.put("blah", bean2);
+    assertThat(MBeans.getMBeanServer().isRegistered(new ObjectName("Test:name=Hello"))).isTrue();
+  }
+
   @Test
   public void testRemove()
   {
@@ -58,16 +75,16 @@ public class TestMMap extends BaseMTest<TestMMap.TestBean>
     testMap.remove("blah");
     assertNotRegistered();
   }
-  
-  @Test 
+
+  @Test
   public void testClear()
   {
     testMap.put("blah", testBean);
     testMap.clear();
     assertNotRegistered();
   }
-  
-  @Test 
+
+  @Test
   public void testPutAll()
   {
     Map<String, TestBean> map = new HashMap<String, TestBean>();
